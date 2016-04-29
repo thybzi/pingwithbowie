987924799457648be368e45aecb5be1d2558a355
Evgeni Dmitriev <thybzi@gmail.com>
2016-04-29 22:23:35 +0300

    Add stat api method

--- bowie/views.py
+++ bowie/views.py
@@ -2,6 +2,7 @@
 from bowie import app
 from bowie import storage
 from bowie import twitter
+from flask import request
 import json
 import calendar
 
@@ -83,6 +84,103 @@ def collections():
                 'tweet_timestamp': tweet_timestamp,
                 'tweet_content': tweet_content,
             })
+
+    # Output JSON results
+    try:
+        return json.dumps(result, ensure_ascii=False).encode('utf8')
+    except:
+        return api_error('Result output error', 500)
+
+
+@app.route('/api/stat/')
+def stat():
+    """Return statistical data for previous and recent tweet collections
+
+    Example:
+        /api/stat/
+        /api/stat/?slowest=10
+    """
+
+    # Get slowest words output count
+    try:
+        slowest = int(request.args.get('slowest'))
+        if slowest < 1:
+            raise ValueError
+    except:
+        slowest = 5
+
+    # Get words list
+    try:
+        words = storage.get_words()
+    except:
+        return api_error('Storage error when getting words list', 301)
+
+    # Get recent collection
+    try:
+        recent_data = storage.get_recent_collection()
+    except:
+        recent_data = []
+
+    # Get prev collection
+    try:
+        prev_data = storage.get_prev_collection()
+    except:
+        prev_data = []
+
+    # Output data
+    result = {}
+
+    # Process tweets data
+    for key, data in {'recent': recent_data, 'prev': prev_data}.items():
+
+        prev_tweet_timestamp = None
+        tweets_data = []
+
+        for i, item in enumerate(data):
+
+            # Parse tweet data
+            try:
+                tweet_data = json.loads(item)
+            except:
+                return api_error('Tweet data error', 400)
+
+            # Build tweet URL
+            try:
+                tweet_url = twitter.get_tweet_url(tweet_data)
+            except:
+                return api_error('Tweet data error', 401)
+
+            # Parse and format tweet time
+            try:
+                tweet_time_struct = twitter.parse_twitter_time(tweet_data['created_at'])
+                tweet_timestamp = int(calendar.timegm(tweet_time_struct))
+                tweet_time = twitter.get_formatted_datetime(tweet_time_struct)
+            except:
+                return api_error('Tweet data error', 403)
+
+            # Consider first tweet was collected immediately (no prev tweet data)
+            if i == 0:
+                prev_tweet_timestamp = tweet_timestamp
+
+            # Append item to results list
+            tweets_data.append({
+                'word': words[i],
+                'word_index': i,
+                'tweet_url': tweet_url,
+                'tweet_time': tweet_time,
+                'tweet_timestamp': tweet_timestamp,
+                'tweet_time_delta': tweet_timestamp - prev_tweet_timestamp,
+            })
+
+            prev_tweet_timestamp = tweet_timestamp
+
+        # Save result item
+        result[key] = {
+            'first_tweet_time': tweets_data[0]['tweet_time'],
+            'last_tweet_time': tweets_data[-1]['tweet_time'],
+            'collect_duration': tweets_data[-1]['tweet_timestamp'] - tweets_data[0]['tweet_timestamp'],
+            'slowest_words': list(sorted(tweets_data, key=lambda item: -item['tweet_time_delta'])[:slowest]),
+        }
 
     # Output JSON results
     try:
